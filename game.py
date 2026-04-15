4c2a203d654502078e8e7130b73d336b9a354961
Rybydak <matvei.rybydak@yandex.ru>
2026-04-15 22:08:37 +0300

    Доработали код в game.py

--- game.py
+++ game.py
@@ -2,21 +2,14 @@
 
 from gameparts import Board
 
+# Вот новая функция.
+def main():
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display()
 
-
-
+# А вот вызов этой функции.
 if __name__ == '__main__':
-    game = Board()
-    game.display()
-    game.make_move(1, 1, 'X')
-    print('Ход сделан!')
-    game.display() 
-    # Создать игровое поле - объект класса Board.
-    game = Board()
-    # Отрисовать поле в терминале.
-    game.display()
-    # Разместить на поле символ по указанным координатам - сделать ход.
-    game.make_move(1, 1, 'X')
-    print('Ход сделан!')
-    # Перерисовать поле с учётом сделанного хода.
-    game.display()
+    main()
