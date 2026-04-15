7834bf091f67366118b9735186abebb03ab2a185
Rybydak <matvei.rybydak@yandex.ru>
2026-04-15 22:13:53 +0300

    переопределили метод __str__

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,8 +1,14 @@
-# parts.py
+# gameparts/parts.py
 
 class Board:
+
+    # Новый атрибут.
+    field_size = 3
+
     def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+        self.board = [
+            [' ' for _ in range(self.field_size)] for _ in range(self.field_size)
+        ]
 
     def make_move(self, row, col, player):
         self.board[row][col] = player
@@ -11,3 +17,10 @@ class Board:
         for row in self.board:
             print('|'.join(row))
             print('-' * 5)
+
+    # Переопределяем метод __str__.
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
