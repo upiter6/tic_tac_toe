2dbc515327afbf015ab267ba145f93f8005c1734
Rybydak <matvei.rybydak@yandex.ru>
2026-04-15 22:21:09 +0300

    дописали код для исключения

--- game.py
+++ game.py
@@ -1,14 +1,22 @@
 # game.py
 
 from gameparts import Board
+# Из файла exceptions.py, который лежит в пакете gameparts,
+# импортируется класс FieldIndexError.
+from gameparts.exceptions import FieldIndexError
 
 def main():
     game = Board()
     game.display()
-    # Тут пользователь вводит координаты ячейки.
+    # Пользователь вводит номер строки.
     row = int(input('Введите номер строки: '))
+    # Если введённое значение меньше нуля или больше или равно
+    # field_size (это значение равно трём, оно хранится в модуле
+    # parts.py)...
+    if row < 0 or row >= game.field_size:
+        # ...выбросить исключение FieldIndexError.
+        raise FieldIndexError
     column = int(input('Введите номер столбца: '))
-    # В метод make_move передаются те координаты, которые ввёл пользователь.
     game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
