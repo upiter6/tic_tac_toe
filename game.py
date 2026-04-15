47bc8eef71c6dac7e32bcf8d058158c8258cbb88
Rybydak <matvei.rybydak@yandex.ru>
2026-04-15 22:07:43 +0300

    изменение добавили if __name__ == '__main__'

--- game.py
+++ game.py
@@ -2,17 +2,21 @@
 
 from gameparts import Board
 
-game = Board()
-game.display()
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-game.display() 
-# Создать игровое поле - объект класса Board.
-game = Board()
-# Отрисовать поле в терминале.
-game.display()
-# Разместить на поле символ по указанным координатам - сделать ход.
-game.make_move(1, 1, 'X')
-print('Ход сделан!')
-# Перерисовать поле с учётом сделанного хода.
-game.display()
+
+
+
+if __name__ == '__main__':
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display() 
+    # Создать игровое поле - объект класса Board.
+    game = Board()
+    # Отрисовать поле в терминале.
+    game.display()
+    # Разместить на поле символ по указанным координатам - сделать ход.
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    # Перерисовать поле с учётом сделанного хода.
+    game.display()
