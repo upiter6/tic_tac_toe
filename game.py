dd5a99b3472e69101eff3394a7687333735a9db7
Rybydak <matvei.rybydak@yandex.ru>
2026-04-20 20:23:20 +0300

    Выполнили изменение кода

--- game.py
+++ game.py
@@ -1,18 +1,16 @@
 # game.py
 
 from gameparts import Board
-from gameparts.exceptions import FieldIndexError
+# Добавился ещё один импорт - исключение CellOccupiedError.
+from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 
 def main():
     game = Board()
-    # Первыми ходят крестики.
     current_player = 'X'
-    # Это флаговая переменная. По умолчанию игра запущена и продолжается.
     running = True
     game.display()
 
-    # Тут запускается основной цикл игры.
     while running:
 
         print(f'Ход делают {current_player}')
@@ -26,12 +24,19 @@ def main():
                 column = int(input('Введите номер столбца: '))
                 if column < 0 or column >= game.field_size:
                     raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    # Вот тут выбрасывается новое исключение.
+                    raise CellOccupiedError
             except FieldIndexError:
                 print(
                     'Значение должно быть неотрицательным и меньше '
                     f'{game.field_size}.'
                 )
                 print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты.')
                 continue
             except ValueError:
                 print('Буквы вводить нельзя. Только числа.')
@@ -42,13 +47,8 @@ def main():
             else:
                 break
 
-        # Теперь для установки значения на поле само значение берётся
-        # из переменной current_player.
         game.make_move(row, column, current_player)
         game.display()
-        # Тернарный оператор, через который реализована смена игроков.
-        # Если current_player равен X, то новым значением будет O,
-        # иначе — новым значением будет X.
         current_player = 'O' if current_player == 'X' else 'X'
 
 
