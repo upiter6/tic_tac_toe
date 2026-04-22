20e41fd69003d4a69ed02943202af6742a1008b6
Rybydak <matvei.rybydak@yandex.ru>
2026-04-22 19:39:36 +0300

    Нарисовали линии и квадрат (1 практрика)

--- gamegraphic.py
+++ gamegraphic.py
@@ -20,7 +20,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+# Рисование линии.
+    pygame.draw.line(screen, (255, 0, 0), (100, 100), (700, 500), 5)
 
+    # Рисование квадрата.
+    # Квадрат с верхним левым углом в точке (300, 200) и размерами 200x200 
+    # будет нарисован зелёным цветом.
+    pygame.draw.rect(screen, (0, 128, 0), pygame.Rect(300, 200, 200, 200))
+
+    # Отобразить нарисованные элементы в окне.
+    pygame.display.update()
 
 # Деинициализирует все модули pygame, которые были инициализированы ранее.
 pygame.quit()
